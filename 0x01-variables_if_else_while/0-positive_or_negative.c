66e8102eeb0e0e794b5fe9732e87f66c194926bf
bunconice <bunconice@gmail.com>
2023-06-16 19:35:35 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,13 +5,13 @@
 
 /*
  * File: 0-positive_or_negative.c
- * Author: Bunm
+ * Author: Bunmi
  * Description: This program generates a random number and prints
  * whether it is positive, negative, or zero.
  */
-/*
- * main - Entry point of the program
- * assign a random number to the variable 'n' and determines if it is positive, negative, or zero
+/**
+ * main -  assign a random number to the variable 'n' 
+ * and determines if it is positive, negative, or zero
  * Return: It returns 0 to indicate successful execution.
  */
 int main(void)
