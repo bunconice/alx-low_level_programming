8e5df9f58a16a1c608f35221047a1ea9d948091e
bunconice <bunconice@gmail.com>
2023-06-21 09:07:38 -0700

    a program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 
 /**
- * main - print first 50 Fibonacci numbers
+ * main - main block
+ * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
+ * Numbers must be coma and space separated.
  * Return: 0
  */
 
 int main(void)
 {
-	int counter;
-	int countto = 50;
-	long a = 1;
-	long b = 2;
+    int count = 3; /* this is so because the first 2 members have been given already. My loop will therefore begin from the 3rd one */
 
-	for (counter = 1; counter <= (countto / 2); counter++)
-	{
-		printf("%li,%li", a, b);
-		a += b;
-		b += a;
-	}
-	if (countto % 2 == 1)
-		printf("%li", a);
+    long int first = 1, second = 2;
+    long int next = first + second;
 
-	printf("\n");
+    printf("%lu, ", first);
+    printf("%lu, ", second);
 
-	return (0);
+    while (count <= 50)
+    {
+        /* Let's check if we are at the end of the list, if we are close with a new line */
+        if (count == 50)
+        {
+            printf("%lu \n", next);
+         }
+         else  /* if we are not at the end of the list, add a comma after the number */
+         { 
+           printf("%lu, ", next); 
+         }
+
+        /* Reset the variables to get the next number */
+        first = second;
+        second = next;
+
+        /* after resetting the variables, you need to find the next number */
+        next = first + second; 
+        count++;
+    }
+
+    return (0);
 }
+
