2d96ac7264153591ebc8c2b51a2900660cd3eb0b
bunconice <bunconice@gmail.com>
2023-06-23 01:49:53 -0700

    function mutiplies two interger

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - function multiplies two integers
+ * @a: first input
+ * @b: second input
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
