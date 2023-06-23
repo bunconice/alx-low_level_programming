ed4c923976816a0dfc687be3e64d0482add5f1ec
bunconice <bunconice@gmail.com>
2023-06-23 03:04:35 -0700

    prints 0-9, without including 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * print_numbers - prints number 0-9
+ * print_most_numbers - prints number 0-9
  * but jumps 2 and 4
  * return: 0-9 with 4 and 2
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
