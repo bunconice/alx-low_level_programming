7e6d2b1d8b83c2dc6b9070b6042d8d0c70744a3b
bunconice <bunconice@gmail.com>
2023-07-07 16:27:03 -0700

    added a new prototype

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-int main(int argc, char *argv[]);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
