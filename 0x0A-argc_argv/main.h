620be8e19acaa95924c67467bc6fc4af08bb44cd
bunconice <bunconice@gmail.com>
2023-07-07 15:55:43 -0700

    initial commit

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+bool is_num(char *argvv);
+int main(int argc, char *argv[]);
+
+#endif
