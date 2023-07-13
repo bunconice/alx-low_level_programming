fd30830c4c281b7906b3d5240af7fd5fd3529ae4
bunconice <bunconice@gmail.com>
2023-07-13 12:22:42 -0700

    function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: integer to determine size of malloc
+ * Return: pointer to malloc
+ */
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;	
+	void *ptr;
+
 	ptr = malloc(b);
 	if (ptr == NULL)
 		exit(98);
