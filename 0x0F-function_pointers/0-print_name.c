8909672b4da8dc73eba0f18688cc5f33da62e9f2
Bunmi Akinola <bunconice@gmail.com>
2023-07-20 16:52:08 -0700

    initial commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints name
- * @name: first parameter
- * @f: second parameter
- * */
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to a function
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
