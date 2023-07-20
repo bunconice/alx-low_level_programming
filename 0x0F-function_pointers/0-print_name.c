e664905b67ae2a8a16e579eedc4b7e9b8606eac9
Bunmi Akinola <bunconice@gmail.com>
2023-07-20 16:39:07 -0700

    initial commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,11 @@
 /**
  * print_name - function that prints name
  * @name: first parameter
- * @(*f): second parameter
+ * @f: second parameter
  * */
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
