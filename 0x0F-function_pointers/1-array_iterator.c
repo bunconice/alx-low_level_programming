8a8d0d679013c23a1a781144aa052f932411979f
Bunmi Akinola <bunconice@gmail.com>
2023-07-20 17:31:06 -0700

    solution to task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through an array
+ * @array: array of numbers
+ * @size: size of the array
+ * @action: prints elements of the array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && action)
+	{
+		for (int i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
