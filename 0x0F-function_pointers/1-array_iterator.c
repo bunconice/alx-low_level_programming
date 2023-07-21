89ff95d955abcb247286483b726501d90ce8514a
Bunmi Akinola <bunconice@gmail.com>
2023-07-20 17:46:47 -0700

    solution to task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array && action)
+	int i;
+	
+	if (array != NULL && action != NULL)
 	{
-		for (int i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 			action(array[i]);
 	}
 }
