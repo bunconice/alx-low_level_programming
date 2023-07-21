b85d997eafa87968f752f489b2eb7d8016410e9c
Bunmi Akinola <bunconice@gmail.com>
2023-07-20 19:10:54 -0700

    solution to task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -14,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
+	if (array && cmp && size)
 	{
 		for (i = 0; i < size; i++)
 		{
