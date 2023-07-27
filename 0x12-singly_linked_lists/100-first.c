b581155b350d2295aadedc5be2d3e28d703581b4
Bunmi Akinola <bunconice@gmail.com>
2023-07-27 15:22:05 -0700

    solution to task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void beforeMain(void)__attribute__((constructor));
+/**
+ * beforeMain - prints a sentence before the main
+ * function is executed
+ */
+
+void beforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
