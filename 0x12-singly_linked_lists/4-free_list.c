c471c2cfbf1fa4167b2ce4e1ae66a16faa1bf184
Bunmi Akinola <bunconice@gmail.com>
2023-07-27 15:16:12 -0700

    solution to task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *temp; /*Declare a pointer to a temporary node*/
+
+	while (head)
+	{
+		temp = head->next; /*Set the temporary pointer to the next node in the list*/
+		free(head->str);/*Free memo allocated for str field of current node*/
+		free(head);/*Free memo allocated for current node*/
+		head = temp;/*Set h ptr to point to next node in list*/
+	}
+}
