266260ec8b0dbf9d1ab5c3df02e96f79f74a48af
Bunmi Akinola <bunconice@gmail.com>
2023-08-03 13:43:03 -0700

    fixed error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,25 +1,16 @@
 #include "main.h"
 
-#include "holberton.h"
-
 /**
- * get_bit - get bit at index
- * @n: number
- * @index: index within binary number
- * Return: bit 0 or 1, or -1 if error
+ * get_bit - returns the value of a bit at a given index
+ * @n: unsigned long int to search
+ * @index: index to return
+ *
+ * Return: the value of the bit at index or -1 if error
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit;
-	unsigned int max_bits;
-
-	/* validate index is not out of range */
-	max_bits = (sizeof(unsigned long int) * 8);
-	if (index > max_bits)
-		return (-1);
-
-	/* shift number index places right to find bit */
-	bit = ((n >> index) & 1);
-
-	return (bit);
+if (index > 53)
+return (-1);
+return ((n >> index) & 1);
 }
