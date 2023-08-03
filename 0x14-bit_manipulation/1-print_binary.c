1687d657782f8d125577298b669b155c6af23a20
Bunmi Akinola <bunconice@gmail.com>
2023-08-03 13:29:52 -0700

    updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,35 +1,12 @@
 #include "main.h"
-
-#include "holberton.h"
-#include <stdio.h>
 /**
- * print_binary - converts unsigned int to binary
- * @n: unsigned int
- * Return: binary
+ * print_binary - prints the binary representation of a number
+ * @n: unsigned long, number to be represented in binary
  */
+
 void print_binary(unsigned long int n)
 {
-
-	unsigned long int n_copy = n, mask = 1;
-	int len = 0;
-
-	while (n_copy > 0)
-	{
-		len++;
-		n_copy >>= 1;
-	}
-	len -= 1;
-
-	if (len > 0) /* create mask based on length of num */
-		mask = mask << len;
-
-	while (mask > 0) /* match each rightmost bit to see if 1 or 0 */
-	{
-		if (n & mask)
-			_putchar('1');
-		else
-			_putchar('0');
-
-		mask >>= 1;
-	}
+if (n > 1)
+print_binary(n >> 1);
+(n & 1) ? _putchar('1') : _putchar('0');
 }
