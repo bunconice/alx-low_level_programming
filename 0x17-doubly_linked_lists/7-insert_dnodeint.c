c9a81290ee08040e6a4b5e814ded97f716760f49
Bunmi Akinola <bunconice@gmail.com>
2023-09-14 22:30:10 +0100

    inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,50 @@
-#include"lists.h"
+#include "lists.h"
 /**
- * insert_dnodeint_at_index - function inserts a node at a given position
- * @h: head of the doubly linked list
- * @idx: position to add the new node
- * @n: data to add to the new node
- * Return: the address of the new node, or NULL if it failed
-*/
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: ptr tp the first node
+ * @idx: new node pos
+ * @n: data
+ * Return: NewN
+ **/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *tmp = *h;
+	dlistint_t *newN = malloc(sizeof(dlistint_t));
+	dlistint_t *c = *h;
 	unsigned int i = 0;
 
 	if (h == NULL)
 		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
 
+	if (newN == NULL)
+		return (NULL);
+
+	newN->n = n;
+	newN->next = NULL;
+	newN->prev = NULL;
 	if (idx == 0)
 	{
 		if (*h != NULL)
-			(*h)->prev = new_node;
-		new_node->next = *h;
-		*h = new_node;
+			(*h)->prev = newN;
+		newN->next = *h;
+		*h = newN;
+		return (newN);
 	}
-
-	while (i < idx - 1)
-	{
-		if (tmp == NULL)
+		while (c != NULL && i < idx - 1)
+		{
+			c = c->next;
+			i++;
+		}
+		if (c == NULL)
+		{
+			free(c);
 			return (NULL);
-		tmp = tmp->next;
-		i++;
-	}
-	if (tmp == NULL)
-		return (NULL);
-	new_node->next = tmp->next;
-	new_node->prev = tmp;
-	if (tmp->next == new_node)
-		tmp->next->prev = new_node;
-	tmp->next = new_node;
-	return (new_node);
+		}
+		newN->prev = c;
+		newN->next = c->next;
+		if (c->next != NULL)
+		{
+			c->next->prev = newN;
+		}
+		c->next = newN;
+		return (newN);
 }
